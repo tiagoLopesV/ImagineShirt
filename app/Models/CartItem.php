94a5ffeed3b7d0c45cd1ba3f8276eff82896f650
Tiago Lopes <tjhl644@gmail.com>
2023-06-20 23:14:24 +0100

    Carrinho de Compras
    -pagina funciona para mostrar os items
    
    -funçao remover ja existente, mas ainda não implementada (tem de ser no layout do carrinho)
    
    -função "addItem" já desenvolvida mas ainda não implentada (tem de ser no layout das categories)

---
+++ app/Models/CartItem.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CartItem extends Model
+{
+    use HasFactory;
+
+    public function cart()
+    {
+        return $this->belongsTo(Cart::class);
+    }
+    public function cart()
+    {
+        return $this->belongsTo(Cart::class);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CategorieController;
+use App\Http\Controllers\CartController;
 
 
 Route::view('/', 'home')->name('root');
@@ -12,3 +13,23 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('categorie', [CategorieController::class, 'show'])->name('categorie.show');
+
+Route::get('/cart.cart', [CartController::class, 'show'])->name('cart.show');
+
+
+
+//Cart Routes**************************************************************************
+//add item
+Route::post('/cart/add', [CartController::class, 'addItem'])->name('cart.addItem');
+
+//remove item
+Route::delete('/cart/remove/{itemId}', [CartController::class, 'removeItem'])->name('cart.remove');
+
+//update quantity
+Route::patch('/cart/update/{itemId}', [CartController::class, 'updateQuantity'])->name('cart.update');
+
+//display cart
+//Route::get('/cart', [CartController::class, 'showCart'])->name('cart.show');
+
+//Route::get('/cart', [CartController::class, 'addItem'])->name('cart');
+//*************************************************************************************
